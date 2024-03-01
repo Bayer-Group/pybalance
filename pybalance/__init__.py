0486f74ee55b7c525a6baf1ef64d20f3c799c048
Stephen Privitera <stephen.privitera@bayer.com>
2024-03-01 11:00:18 +0100

    remove version from init file

--- pybalance/__init__.py
+++ pybalance/__init__.py
@@ -4,20 +4,3 @@ import pybalance.propensity
 import pybalance.sim
 import pybalance.visualization
 import pybalance.lp
-
-__version__ = "0.1.1"
-
-import logging
-
-logger = logging.getLogger(__name__)
-logger.info(f"Loaded pybalance version {__version__}.")
-
-# Logging is configured at the application level. To adjust logging level,
-# configure logging as below before importing pybalance:
-#
-# import logging
-# logging.basicConfig(
-#     format="%(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
-#     level='INFO',
-# )
-#
