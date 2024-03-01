9bff7445aa832a81c6a75413d5079af7f11642b9
Stephen Privitera <stephen.privitera@bayer.com>
2024-03-01 14:11:23 +0100

    remove call to deprecated method

--- pybalance/lp/matcher.py
+++ pybalance/lp/matcher.py
@@ -449,7 +449,7 @@ class ConstraintSatisfactionMatcher(object):
 
         if self.verbose:
             solution_printer = SolutionPrinter(x, abs_deltas, self)
-            status = solver.SolveWithSolutionCallback(model, solution_printer)
+            status = solver.Solve(model, solution_printer)
             logger.info("Status = %s" % solver.StatusName(status))
             logger.info(
                 "Number of solutions found: %i" % solution_printer.solution_count()
